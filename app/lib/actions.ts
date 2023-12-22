1368b4ca32844be661fa43505a0b2f3421c20aa3
mauriciosilvera <mauricioagustin_@outlook.com>
2023-12-22 15:00:40 -0300

    Chapter 15 - authentication done.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,9 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
- 
+import { signIn } from '@/auth';
+import { AuthError } from 'next-auth';
+
 const FormSchema = z.object({
   id: z.string(),
   customerId: z.string({
@@ -90,8 +92,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
-
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
@@ -99,5 +99,21 @@ export async function deleteInvoice(id: string) {
     return {
       message: 'Database Error: Failed to delete invoice.'
     }
+  }
+}
+
+export async function authenticate(prevState: string | undefined, formData: FormData) {
+  try {
+    await signIn('credentials', formData);
+  } catch(error) {
+    if(error instanceof AuthError) {
+      switch(error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
   }
 }
